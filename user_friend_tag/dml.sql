3d340a20f4dfa0a80fc7984acbdb2e5bef75b9bc
Adarsh Amit <adarsh.amit1001@gmail.com>
2025-11-18 15:31:00 +0530

    modified dml.sql in user_friend for more clarity

--- user_friend_tag/dml.sql
+++ user_friend_tag/dml.sql
@@ -1,31 +1,38 @@
 INSERT INTO users (name) VALUES
-('user1'),
-('user2'),
-('user3'),
-('user4'),
-('user5');
+  ('user1'),
+  ('user2'),
+  ('user3'),
+  ('user4'),
+  ('user5')
+  RETURNING *;
 
 
 INSERT INTO friends (user_id, friend_id) VALUES
-(1, 2),(2, 1)
-(1, 3),(3, 1)
-(2, 3),(3, 2)
-(3, 4),(4, 3)
-(4, 5),(5, 4)
+  (1, 2),(2, 1),
+  (1, 3),(3, 1),
+  (2, 3),(3, 2),
+  (3, 4),(4, 3),
+  (4, 5),(5, 4)
+  RETURNING *;
 
-INSERT INTO images (user) VALUES
-(1),
-(1),
-(2),
-(3),
-(4),
+INSERT INTO images (owner) VALUES
+  (1),
+  (1),
+  (2),
+  (2),
+  (3),
+  (4)
+  RETURNING *;
 
 INSERT INTO tags (image_id, tagged) VALUES
-(1, 2),
-(1, 3),
-(2, 1), 
-(3, 2),
-(3, 5),
-(4, 3),
-(5, 4),
-(5, 1);
+  (1, 2),
+  (1, 3),
+  (2, 1), 
+  (3, 2),
+  (3, 5),
+  (3, 1),
+  (4, 1),
+  (4, 3),
+  (5, 4),
+  (5, 1)
+  RETURNING *;
