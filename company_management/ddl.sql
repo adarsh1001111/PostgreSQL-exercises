5e5dcac53e663eee5ff76eefc9e8eee70457bf7a
Adarsh Amit <adarsh.amit1001@gmail.com>
2025-11-18 16:34:52 +0530

    improved user_friend_tag and added company_management exercise

---
+++ company_management/ddl.sql
@@ -0,0 +1,6 @@
+/*
+DROP DATABASE IF EXISTS company;
+CREATE DATABASE company;
+*/
+-- Connect to company database
+\c company;
--- user_friend_tag/dql.sql
+++ user_friend_tag/dql.sql
@@ -3,21 +3,36 @@
 SELECT image_id
 FROM tags
 GROUP BY image_id
-HAVING COUNT(tagged) = (
-    SELECT MAX(tag_count)
-    FROM (
-        SELECT COUNT(tagged) as tag_count
-        FROM tags
-        GROUP BY image_id
-    ) x
-)
-ORDER BY image_id;
+ORDER BY COUNT(tagged) DESC
+LIMIT 1;
 
 --------------------
 --Find all images belonging to the friends of a particular user.
 --for user with name = 'user1'
 
-SELECT id
+EXPLAIN SELECT i.*
+FROM images i JOIN friends f 
+    ON i.owner = f.friend_id
+    JOIN users u ON u.id = f.user_id AND u.name = 'user1';
+
+/*
+                                   QUERY PLAN                                   
+--------------------------------------------------------------------------------
+ Hash Join  (cost=61.85..104.06 rows=109 width=8)
+   Hash Cond: (i.owner = f.friend_id)
+   ->  Seq Scan on images i  (cost=0.00..32.60 rows=2260 width=8)
+   ->  Hash  (cost=61.73..61.73 rows=10 width=4)
+         ->  Hash Join  (cost=25.95..61.73 rows=10 width=4)
+               Hash Cond: (f.user_id = u.id)
+               ->  Seq Scan on friends f  (cost=0.00..30.40 rows=2040 width=8)
+               ->  Hash  (cost=25.88..25.88 rows=6 width=4)
+                     ->  Seq Scan on users u  (cost=0.00..25.88 rows=6 width=4)
+                           Filter: (name = 'user1'::text)
+(10 rows)
+*/
+
+
+EXPLAIN SELECT *
 FROM images
 WHERE owner IN (
     SELECT friend_id
@@ -25,6 +40,22 @@ WHERE owner IN (
     WHERE user_id = (
         SELECT id FROM users WHERE name = 'user1'
     ));
+
+/*
+                             QUERY PLAN                              
+---------------------------------------------------------------------
+ Hash Semi Join  (cost=61.50..101.29 rows=113 width=8)
+   Hash Cond: (images.owner = friends.friend_id)
+   InitPlan 1 (returns $0)
+     ->  Seq Scan on users  (cost=0.00..25.88 rows=6 width=4)
+           Filter: (name = 'user1'::text)
+   ->  Seq Scan on images  (cost=0.00..32.60 rows=2260 width=8)
+   ->  Hash  (cost=35.50..35.50 rows=10 width=4)
+         ->  Seq Scan on friends  (cost=0.00..35.50 rows=10 width=4)
+               Filter: (user_id = $0)
+(9 rows)
+*/
+
 --------------------
 --Find all friends of a particular user (Say, userA) who has tagged him in all of his pics.
 -- for user with name = 'user1'
