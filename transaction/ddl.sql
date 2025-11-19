d5427390815062854d49e81885f7fe58a52a0756
Adarsh Amit <adarsh.amit1001@gmail.com>
2025-11-19 08:40:15 +0530

    updating remotely

---
+++ transaction/ddl.sql
@@ -0,0 +1,29 @@
+/*
+DROP DATABASE IF EXISTS bank;
+CREATE DATABASE bank;
+*/
+
+
+/*
+Connect to bank database
+\c bank;
+*/
+
+DROP TABLE IF EXISTS users, accounts;
+
+CREATE TABLE accounts(
+    id SERIAL PRIMARY KEY,
+    account_number NUMERIC UNIQUE NOT NULL,
+    balance DECIMAL NOT NULL DEFAULT 0.0,
+);
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    account_number NUMERIC UNIQUE NOT NULL REFERENCES accounts(account_number) ON DELETE CASCADE
+);
+
+
+
+
